47b182726b21c846ae8e7efa4e405dd1b4b84a5e
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-06-11 13:37:48 -0400

    Accordion: expand and collapse

--- src/components/ModuleRow.vue
+++ src/components/ModuleRow.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="module-row" :class="{ 'module-row-indented': indent }">
     <div class="module-row-left-column">
-      <FontAwesomeIcon class="d-block mx-auto mb-3" :icon="['fas', 'chevron-down']" size="xl" />
+      <FontAwesomeIcon
+        v-if="$slots['resource-list']"
+        class="d-block mx-auto mb-3 module-row-chevron"
+        :icon="['fas', 'chevron-up']"
+        size="xl"
+        :flip="isExpanded ? 'vertical' : undefined"
+        @click="toggleExpanded"
+      />
       <div class="module-row-icon-container">
         <SvgImage :src="iconSource" class="module-row-icon" />
       </div>
@@ -17,7 +24,7 @@
         >
         <slot></slot>
       </p>
-      <ul v-if="$slots['resource-list']" class="list-unstyled">
+      <ul v-if="$slots['resource-list']" v-show="isExpanded" class="list-unstyled">
         <slot name="resource-list"></slot>
       </ul>
     </div>
@@ -27,6 +34,7 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { SvgImage } from 'vite-awesome-svg-loader/vue-integration';
+import { computed, ref } from 'vue';
 
 defineProps<{
   iconSource: string;
@@ -36,6 +44,24 @@ defineProps<{
   tagline?: string;
   indent?: boolean;
 }>();
+
+const isExpanded = ref(false);
+
+const expand = () => {
+  isExpanded.value = true;
+};
+
+const collapse = () => {
+  isExpanded.value = false;
+};
+
+const toggleExpanded = computed(() => {
+  if (isExpanded.value) {
+    return collapse;
+  } else {
+    return expand;
+  }
+});
 </script>
 
 <style>
@@ -43,6 +69,10 @@ defineProps<{
   padding-top: 1rem;
   margin-bottom: 1rem;
   display: flex;
+}
+
+.module-row-chevron {
+  cursor: pointer;
 }
 
 .module-row-indented {
