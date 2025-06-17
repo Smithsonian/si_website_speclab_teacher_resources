0f61d6f2a3cda0d1208b2c2da391c365ebccc735
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-06-17 12:03:40 -0400

    Google Analytics gtag G-B2HMDCPFNR

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
+import { configure } from 'vue-gtag';
 
 useHead({ title: 'Teacher Resources | Spectrum Lab' });
+
+configure({ tagId: 'G-B2HMDCPFNR' });
 </script>
 
 <template>
