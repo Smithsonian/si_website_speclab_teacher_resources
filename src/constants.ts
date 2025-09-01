fdec0e72eaef4dff1b0375baa74c0f97ba0fb474
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-09-01 16:52:18 -0400

    Fix API base URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
 export const SPECLAB_URL = 'https://datalabs.cfa.harvard.edu/spectrumlab';
 
-const getAPIBaseUrl = () => {
+const getAPIBaseUrl = (): string => {
   if (import.meta.env.DEV) {
     return '/api';
   }
-  return `${import.meta.env.BASE_URL}../api`;
+  const url = new URL('../api', import.meta.env.BASE_URL);
+  return url.toString();
 };
 
 export const API_BASE_URL = getAPIBaseUrl();
